1f69538f3432764ffd7bd4c12f677a6b0f618884
Henry Gelderbloem <henry.gelderbloem@waterstons.com>
2022-03-02 11:28:28 +0000

    Fixes an issue with getting the execution policy

--- AVD/AIB/installFsLogix.ps1
+++ AVD/AIB/installFsLogix.ps1
@@ -1,4 +1,6 @@
 Write-Host 'AIB Customisation: Downloading FsLogix'
+$Policy = Get-ExecutionPolicy -List 
+Write-Host $Policy
 New-Item -Path C:\\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
 $LocalPath = 'C:\\fslogix'
 $flogixURL = 'https://raw.githubusercontent.com/HenryGelderbloem/Azure-IaC/main/AVD/AIB/fsLogixSetup.ps1'
@@ -15,8 +17,6 @@ Expand-Archive $LocalPath\$installerFile -DestinationPath $LocalPath
 Write-Host 'AIB Customisation: Download Fslogix installer finished'
 
 Write-Host 'AIB Customisation: Start Fslogix installer'
-$Policy = Get-ExecutionPolicy -List 
-Write-Host $Policy
 Set-ExecutionPolicy -Scope:CurrentUser -ExecutionPolicy:Unrestricted
 .\\FSLogixSetup.ps1 
 Write-Host 'AIB Customisation: Finished Fslogix installer' 
